d09de4cacd509e4298f4ba5f5df410d7cf204d49
Gabriel Jordan <gjordanp@gmail.com>
2023-07-23 09:56:04 -0400

    Custom Error ultimos ajustes

--- src/middlewares/autorization.js
+++ src/middlewares/autorization.js
@@ -1,13 +1,27 @@
+import CustomError from "../services/errors/customError.js";
+import EErrors from "../services/errors/enumError.js";
 const autorization = (role) => {
     return (req, res, next) => {
         //console.log("authorization",req.session.user);
         if (!req.session.user) {//Si el usuario no esta logueado
-            return res.status(401).send({ message: "Debes iniciar session para realizar esta acción" });
+            //return res.status(401).send({ message: "Debes iniciar session para realizar esta acción" });
+            CustomError.createError({ 
+                name: "Unauthenticated", 
+                cause: "User not logged",
+                message: "Debes iniciar session para realizar esta acción", 
+                code: EErrors.AUTENTICATION_ERROR
+            });
+
         } else {//Si el usuario esta logueado
             if (req.session.user.role == role) {
                 next();
             } else {
-                return res.status(403).send({ message: "No tienes permisos para realizar esta acción" });
+                CustomError.createError({ 
+                    name: "Unauthorized", 
+                    cause: "User role not allowed",
+                    message: "No tienes permisos para realizar esta acción", 
+                    code: EErrors.AUTENTICATION_ERROR
+                });
             }
         }
     }
--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -3,16 +3,22 @@ export default (error, req, res, next) => {
     console.log(error.cause);
     switch (error.code) {
         case EErrors.ROUTING_ERROR:
-            res.status(404).send({ status: "error", error:error.name });
+            res.status(404).send({ status: "Error", error:error.name, message: error.message });
             break;
         case EErrors.INVALID_TYPE_ERROR:
-            res.status(401).send({ status: "error", error:error.name });
+            res.status(401).send({ status: "Error", error:error.name, message: error.message });
             break;
         case EErrors.DATABASE_ERROR:
-            res.status(500).send({ status: "error", error:error.name });
+            res.status(500).send({ status: "Error", error:error.name, message: error.message });
+            break;
+        case EErrors.AUTENTICATION_ERROR:
+            res.status(401).send({ status: "Error", error:error.name, message: error.message });
+            break;
+        case EErrors.AUTORIZATION_ERROR:
+            res.status(403).send({ status: "Error", error:error.name, message: error.message });
             break;
         default:
-            res.status(500).send({ status: "error", error: "Error indeterminado" });
+            res.status(500).send({ status: "Error", error: "Error indeterminado" });
             break;
     }
 }
